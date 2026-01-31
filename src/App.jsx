267f78bb7d15b867eb5cfe708b509ddd7c01af4b
Swapnil Saurabh <swapnilsaurabh@Swapnils-MacBook-Air.local>
2026-01-31 14:05:02 +0530

    added authentication routing to app .js

--- src/App.jsx
+++ src/App.jsx
@@ -7,25 +7,25 @@ import { Toaster } from "react-hot-toast";
 import { AuthContext } from "../context/AuthContext";
 
 const App = () => {
-  //const { authUser, loading } = useContext(AuthContext);
+  const { authUser, loading } = useContext(AuthContext);
 
-  //if (loading) return <div className="text-center p-10">Loading...</div>;
+  if (loading) return <div className="text-center p-10">Loading...</div>;
 
   return (
-    <div className="bg-[url('./assets/bgImage.svg')] bg-contain ">
-      <Toaster/>
+    <div className="bg-[url('/bgImage.svg')] bg-contain ">
+      <Toaster />
       <Routes>
         <Route
           path="/"
-          element={ <HomePage /> }
+          element={authUser ? <HomePage /> : <Navigate to="/login" />}
         />
         <Route
           path="/login"
-          element={ <LoginPage /> }
+          element={!authUser ? <LoginPage /> : <Navigate to="/" />}
         />
         <Route
           path="/profile"
-          element={<ProfilePage />  }
+          element={authUser ? <ProfilePage /> : <Navigate to="/login" />}
         />
       </Routes>
     </div>
